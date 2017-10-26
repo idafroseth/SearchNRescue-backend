2693982d9fa9c28737425e03e81cc018a0e2f9db
idamfro <idamfro@ulrik.uio.no>
2017-10-26 15:38:27 +0200

    Adding packages

---
+++ searchnrescue/src/main/java/fih/com/searchnrescue/App.java
@@ -0,0 +1,13 @@
+package fih.com.searchnrescue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
